98a89a0ac4945848941a3b0570754b55caf4c71c
Arsen302 <aarakelyan98@gmail.com>
2020-08-02 22:11:24 +0300

    init: start songbird-task

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "./MainPage/MainPage";
+
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
